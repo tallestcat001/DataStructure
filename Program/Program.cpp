5bd94e3d6db01aaee71f5e279b6613ff55fcae56
tallestcat001 <shdmlgus123_@naver.com>
2024-06-10 19:43:32 +0900

    (2024.6.10) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void PushFront(T data)
+    {
+        if (head = nullptr)
+        {
+            head = new Node;
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node* newnode = new Node;
+            newnode->next = head;
+            newnode->data = data;
+            head = newnode;
+        }
+
+        size++;
+
+    }
+    
+    void Show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << endl;
+            currentNode = currentNode->next;
+        }
+    }
+};
+
+int main()
+{
+    SingleLinkedList<int> list;
+    list.PushFront(10);
+    list.PushFront(20);
+    list.Show();
+}
