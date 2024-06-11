6fd762acf34556e8a217037e7726427347a09a35
tallestcat001 <shdmlgus123_@naver.com>
2024-06-11 19:45:15 +0900

    (2024.6.11) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -25,7 +25,7 @@ public:
 
     void PushFront(T data)
     {
-        if (head = nullptr)
+        if (head == nullptr)
         {
             head = new Node;
             head->data = data;
@@ -34,15 +34,84 @@ public:
         else
         {
             Node* newnode = new Node;
-            newnode->next = head;
             newnode->data = data;
+            newnode->next = head;
             head = newnode;
         }
 
         size++;
 
     }
+
+    void PopFront()
+    {
+        if (head != nullptr)
+        {
+            Node* deleteNode = head;
+            head = deleteNode->next;
+            delete deleteNode;
+            size--;
+        }
+        else
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+    }
     
+    void PushBack(T data)
+    {
+        if (head == nullptr)
+        {
+            head = new Node;
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node* currentNode = head;
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            Node* newNode = new Node;
+            currentNode->next = newNode;
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        size++;
+    }
+
+    void PopBack()
+    {
+        if (head != nullptr)
+        {
+            if (size == 1)
+            {
+                Node* deleteNode = head;
+                head = deleteNode->next;
+                delete deleteNode;
+            }
+            else
+            {
+                Node* deletNode = head;
+                Node* previousNode = head;
+                while (deletNode->next != nullptr)
+                {
+                    previousNode = deletNode;
+                    deletNode = deletNode->next;
+                }
+                previousNode->next = deletNode->next;
+                delete deletNode;
+            }
+            size--;
+        }
+        else
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+    }
+
     void Show()
     {
         Node* currentNode = head;
@@ -58,7 +127,11 @@ public:
 int main()
 {
     SingleLinkedList<int> list;
-    list.PushFront(10);
-    list.PushFront(20);
+    list.PushBack(10);
+    list.PushBack(20);
+    list.PushBack(30);
+    list.PopBack();
     list.Show();
+
+    return 0;
 }
