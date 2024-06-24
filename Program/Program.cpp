2ea30cad8680dbfd0a86a0b19237bc7788c5216e
tallestcat001 <shdmlgus123_@naver.com>
2024-06-24 19:47:47 +0900

    (2024.6.24) 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -32,10 +32,68 @@ public:
             bucket[i].head = nullptr;
         }
     }
+
+    template<typename T>
+    int HashFunction(T key)
+    {
+        int hashindex = (int)key % SIZE;
+
+        return hashindex;
+    }
+
+    template<>
+    int HashFunction(std::string key)
+    {
+        int result = 0;
+
+        for (const char& element : key)
+        {
+            result += element;
+        }
+
+        int hashindex = (int)result % SIZE;
+
+        return hashindex;
+    }
+
+    Node* CreatNode(KEY key, VALUE value)
+    {
+        Node *creatnode = new Node();
+        creatnode->key = key;
+        creatnode->value = value;
+        creatnode->next = nullptr;
+
+        return creatnode;
+    }
+
+    void Insert(KEY key, VALUE value)
+    {
+        int hashindex = HashFunction(key);
+
+        Node* newnode = CreatNode(key, value);
+
+        if (bucket[hashindex].count == 0)
+        {
+            bucket[hashindex].head = newnode;
+        }
+        else
+        {
+            newnode->next = bucket[hashindex].head;
+            bucket[hashindex].head = newnode;
+        }
+
+        bucket[hashindex].count++;
+
+    }
+
 };
 
 int main()
 {
-    
+    HashTable<std::string, int> hashtable;
+
+    hashtable.Insert("Sword", 100);
+    hashtable.Insert("Gun", 500);
+
     return 0;
 }
