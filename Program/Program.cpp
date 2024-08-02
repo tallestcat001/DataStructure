a984bd1abb4029956dc0c8332b8468004d7999fc
tallestcat001 <shdmlgus123_@naver.com>
2024-08-02 19:43:56 +0900

    (2024.8.2) 데크

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
 
+#define SIZE 26
+
 using namespace std;
+
+class Trie
+{
+private:
+    bool final;
+
+    Trie* alphabet[SIZE];
+public:
+    Trie()
+    {
+        final = false;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            alphabet[i] = nullptr;
+        }
+
+
+    }
+};
 
 int main()
 {
